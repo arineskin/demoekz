07b41b6bcec29eebfc3b65d3ede5ed367f91bfa9
arineskin <159476085+arineskin@users.noreply.github.com>
2026-05-28 22:50:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const images = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg'];
 let current = 0;
-const img = document.getElementById('slider-img');
+const img = document.getElementById('slide');
 
-if (img) {
-    setInterval(function() {
-        current = (current + 1) % images.length;
-        img.src = images[current];
-    }, 3000);
+function next() {
+    current = current + 1;
+    if (current >= images.length) current = 0;
+    img.src = images[current];
 }
+
+function prev() {
+    current = current - 1;
+    if (current < 0) current = images.length - 1;
+    img.src = images[current];
+}
+
+document.getElementById('next').onclick = next;
+document.getElementById('prev').onclick = prev;
+setInterval(next, 2000);
